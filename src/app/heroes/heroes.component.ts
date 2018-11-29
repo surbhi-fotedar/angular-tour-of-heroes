ac2f9c41c47dd9655ad7d4f76a5748377a087efa
Surbhi Fotedar <surbhif@CESCHNDES-0108.CESCHN.COM>
2018-11-29 18:30:22 +0530

    late commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,22 +11,30 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  selectedHero: Hero;
-
+ 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
 
-  
-
-  onSelect(hero: Hero): void {
-  this.selectedHero = hero;
+  getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1,5));
 }
 
-getHeroes(): void {
-  this.heroes = this.heroService.getHeroes();
+add(name: string): void {
+  name = name.trim();
+  if (!name) { return; }
+  this.heroService.addHero({ name } as Hero)
+    .subscribe(hero => {
+      this.heroes.push(hero);
+    });
+}
+
+delete(hero: Hero): void {
+  this.heroes = this.heroes.filter(h => h !== hero);
+  this.heroService.deleteHero(hero).subscribe();
 }
 
 }
